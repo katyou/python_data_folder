77156367743131f1518dbb98c72a823250b1ef4e
katyou <mainichiaisatsu@outlook.com>
2016-03-25 18:33:42 +0900

    python3 ver

--- solar.py
+++ solar.py
@@ -1,23 +1,29 @@
-import serial
-import os
-import shutil
-import datetime
-import time
+while True:
 
-ser = serial.Serial("/dev/ttyACM0",9600)
-time.sleep(2) #arduinoのブート待ち
-ser.write("z") #arduinoに文字列zを送信
-f = open("sample.txt", 'w')
-print("begin to measure")
-oclock = datetime.datetime.today()
-if 5 < oclock.hour < 19:
-    for i in range (1,70):
-        val = ser.readline()
-        print(val)
-        f.write(val)
-f.close()
-time = datetime.datetime.now()
-newname = "{0:%Y%m%d-%H%M%S}.txt".format(time)
-os.rename("sample.txt",newname)
-ser.write("y") #arduinoに文字列yを送信
-ser.close()
+	import serial
+	import os
+	import shutil
+	import datetime
+	import time
+
+	oclock = datetime.datetime.today()
+	if 5 < oclock.hour < 19:
+		ser = serial.Serial("/dev/ttyACM0",9600)
+		time.sleep(2)
+		ser.write(b'z')
+		f = open("sample.txt", 'w')
+		print("begin to measure")
+
+		for i in range (1,70):
+			val = ser.readline()
+			print(val.strip().decode('utf-8'))
+		f.write(val.strip().decode('utf-8'))
+		f.close()
+		time = datetime.datetime.now()
+		newname = "{0:%Y%m%d-%H%M%S}.txt".format(time)
+		os.rename("sample.txt",newname)
+
+		ser.write(b"y")
+		import time
+		time.sleep(30.0)
+		ser.close()
