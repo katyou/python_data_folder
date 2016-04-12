41e7bee9624ad5dffe766f3d48b2b7be1e35992c
katyou <mainichiaisatsu@outlook.com>
2016-04-12 12:01:37 +0900

    separate folder

--- solar.py
+++ solar.py
@@ -1,13 +1,22 @@
 while True:
-
+	import os
+	import datetime
+	import time
 	import serial
 	import os
 	import shutil
-	import datetime
-	import time
+
+	os.mkdir("sample")
 
 	oclock = datetime.datetime.today()
-	if 5 < oclock.hour < 19:
+
+	while 6 <= oclock.hour <= 20:
+		import serial
+		import os
+		import shutil
+		import time
+
+		os.chdir("/home/pi/Desktop/python_programing/sample") #change directly
 		ser = serial.Serial("/dev/ttyACM0",9600)
 		time.sleep(2)
 		ser.write(b'z')
@@ -23,17 +32,38 @@ while True:
 
 		from matplotlib import pyplot as plt
 		import numpy as np
-		x,y = np.loadtxt('sample.txt',delimiter=',',unpack=True)
+
+		x,y = np.loadtxt('sample.txt',delimiter = ',',unpack = True)
 		plt.plot(x,y)
 		plt.savefig('sample.png')
 
-		time = datetime.datetime.now()
-		newname = "{0:%Y-%m-%d-%H-%M:%S}.png".format(time)
+		dailytime = datetime.datetime.now()
+		newname = "{0:%Y-%m-%d-%H-%M:%S}.png".format(dailytime)
 		os.rename("sample.png",newname)
+		
 		import os
 		os.remove('sample.txt')
 
 		ser.write(b"y")
-		import time
-		time.sleep(30.0)
+		time.sleep(10.0)
 		ser.close()
+
+	while oclock.hour > 20:
+		time.sleep(60)
+
+	while oclock.hour < 5:
+		time.sleep(60)
+
+
+def png():   #sub function 
+	import os
+	from matplotlib import pyplot as plt
+	import numpy as np
+
+	x,y = np.loadtxt('sample.txt',delimiter = ',',unpack = True)
+	plt.plot(x,y)
+	plt.savefig('sample.png')
+
+	dailytime = datetime.datetime.now()
+	newname = "{0:%Y-%m-%d-%H-%M:%S}.png".format(dailytime)
+	os.rename("sample.png",newname)
