2bf495bb77de4fce7d5cf89c641e8a8915521116
katyou <mainichiaisatsu@outlook.com>
2016-07-13 15:23:38 +0900

    20160713

---
+++ arduino/arduino.ino
@@ -0,0 +1,51 @@
+void setup()
+{
+  pinMode(8, OUTPUT);
+  Serial.begin(9600);
+}
+
+
+void loop()
+{
+  char a;
+  if (Serial.available() > 0)
+  {
+    a = Serial.read();
+    if (a == 'z')
+    {
+      delay(20);
+      digitalWrite(8,LOW);
+      Measure();
+    }
+    if (a == 'y')
+    {
+      digitalWrite(8,HIGH);
+    }
+  }
+}
+
+void Measure()
+{
+  float Vi;
+  float Im;
+  float v[100];
+  float i[100];
+  int x;
+  for(x = 1; x <= 50; x++)
+  {
+    Vi = analogRead(A3);
+    v[x] = Vi;
+    Im = analogRead(A5);
+    i[x] = Im;
+    
+    delayMicroseconds(1000);
+  }
+  delay(5000);
+  for(x = 1; x <= 50; x++)
+  {
+    Serial.print(v[x]);
+    Serial.print(',');
+    Serial.println(i[x]);
+  }
+}
+
--- solar.py
+++ solar.py
@@ -23,18 +23,20 @@ while True:
 		serial = open("sample.txt", 'w')
 		print("begin to measure")
 
-		for i in range (1,70):
+		for i in range (1,50):
 			val = ser.readline()
 			print(val.decode('utf-8'))
 			serial.write(val.decode('utf-8'))
 
 		serial.close()
 
+		ser.write(b"y")
 
 		#text file caluculate
 		from matplotlib import pyplot as plt
 		import numpy as np
 
+		fig = plt.figure()
 		data = np.loadtxt('sample.txt',delimiter = ',',unpack = True)
 
 		plot1 = data[:,0]
@@ -47,7 +49,6 @@ while True:
 		plt.savefig('sample.png')
 		#text file calculate finish
 
-
 		dailytime = datetime.datetime.now()
 		newname = "{0:%Y-%m-%d-%H-%M:%S}.png".format(dailytime)
 		os.rename("sample.png",newname)
@@ -55,13 +56,12 @@ while True:
 		import os
 		os.remove('sample.txt')
 
-		os.chdir("/home/ienaga/デスクトップ/uploader/public/graph") #change directly
-		plt.savefig('sample.png')
+		#os.chdir("/home/ienaga/デスクトップ/uploader/public/graph") #change directly
+		#plt.savefig('sample.png')
 		
-		ser.write(b"y")
 		time.sleep(10.0)
-		ser.close()
 
+		ser.close()
 
 	while oclock.hour > 20:
 		os.chdir("/home/pi/Desktop/python_programing") #change directly
@@ -72,7 +72,7 @@ while True:
 		time.sleep(60)
 
 	while oclock.hour < 5:
-		time.sleep(60)
+		time.sleep(120)
 
 
 def png():   #sub function 
